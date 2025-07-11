6b010a28ce17a623940491f2f31fd921684602d3
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-07-11 10:20:29 +0300

    playwright authentication

--- valentino-magic-beans/tests/setup/auth.setup.ts
+++ valentino-magic-beans/tests/setup/auth.setup.ts
@@ -1,7 +1,29 @@
-import { test as setup, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
+import * as loginPage from '../pages/Login'
 import path from 'path';
+import fs from 'fs';
 
-setup('authenticate', async ({ page }) => { 
+const authSessionFile = path.resolve(__dirname, '../../playwright/.auth/user.json');
+
+// Read and parse the JSON file
+const loginDataFile = path.resolve(__dirname, '../../playwright/.auth/loginData.json');
+const loginData = JSON.parse(fs.readFileSync(loginDataFile, 'utf-8')) as {
+    email: string,
+    pass: string
+}
+
+test('authenticate', async ({ page }) => { 
+    await page.goto('/login')
+    await loginPage.login(
+        page,
+        loginData.email,
+        loginData.pass
+    )
+    await loginPage.verifySuccessfulLogin(page)
+
+    await page.context().storageState({
+        path: authSessionFile
+    })
 
 
 
