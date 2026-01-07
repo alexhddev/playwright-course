e1b23201d52f67cecbe8ec5dcd816357ab067c6f
alexhddev <21276888+alexhddev@users.noreply.github.com>
2026-01-07 13:58:55 +0000

    GetBYRole - buttons and links

--- Starter/tests/locators/1GetByRole.spec.ts
+++ Starter/tests/locators/1GetByRole.spec.ts
@@ -22,5 +22,40 @@ test('Get by Role - list', async ({ page }) =>{
         const itemText = await item.textContent()
         expect(itemText).toBeTruthy()
     }
-
 })
+
+test('Get by Role - Buttons', async ({ page }) =>{
+    await page.goto('') 
+
+    const acceptCookiesButton = page.getByRole('button', {
+        name: 'Accept',
+        exact: true
+    })
+    const declineCookiesButton = page.getByRole('button', {
+        name: 'decline'
+    })
+
+    await acceptCookiesButton.click()
+
+    await expect(acceptCookiesButton).not.toBeVisible()
+    await expect(declineCookiesButton).not.toBeVisible()
+})
+
+test('Get by Role - link', async ({ page }) =>{
+    await page.goto('')  
+
+    await page.getByRole('button', {
+        name: 'decline'
+    }).click()
+    
+    await page.getByRole('link', {
+        name: 'Go to Feedback Form'
+    }).click()
+
+    const url = page.url()
+    expect(url).toContain('FeedBack')
+})
+
+
+
+
